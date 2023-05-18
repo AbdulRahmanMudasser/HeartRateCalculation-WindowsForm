59db49b1504f22ce174e64879acee33d4bbad210
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-05-19 00:18:01 +0500

    solution done

--- Heart Rate Calculation/Form1.Designer.cs
+++ Heart Rate Calculation/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.secondNameTextBox = new System.Windows.Forms.TextBox();
+            this.birthYearTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Second Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(290, 55);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(100, 26);
+            this.firstNameTextBox.TabIndex = 2;
+            // 
+            // secondNameTextBox
+            // 
+            this.secondNameTextBox.Location = new System.Drawing.Point(290, 129);
+            this.secondNameTextBox.Name = "secondNameTextBox";
+            this.secondNameTextBox.Size = new System.Drawing.Size(100, 26);
+            this.secondNameTextBox.TabIndex = 3;
+            // 
+            // birthYearTextBox
+            // 
+            this.birthYearTextBox.Location = new System.Drawing.Point(290, 201);
+            this.birthYearTextBox.Name = "birthYearTextBox";
+            this.birthYearTextBox.Size = new System.Drawing.Size(100, 26);
+            this.birthYearTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Year of Birth";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(223, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 34);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.birthYearTextBox);
+            this.Controls.Add(this.secondNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox secondNameTextBox;
+        private System.Windows.Forms.TextBox birthYearTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Heart Rate Calculation/Form1.cs
+++ Heart Rate Calculation/Form1.cs
@@ -16,5 +16,64 @@ namespace Heart_Rate_Calculation
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        class HeartRate
+        {
+            private string firstName;
+            private string secondName;
+            private int birthYear;
+            private int currentYear;
+
+            public HeartRate(string firstName, string secondName, int birthYear)
+            {
+                FirstName = firstName;
+                SecondName = secondName;
+                BirthYear = birthYear;
+                CurrentYear = DateTime.Now.Year;
+            }
+
+            public string FirstName { get => firstName; set => firstName = value; }
+            public string SecondName { get => secondName; set => secondName = value; }
+            public int BirthYear { get => birthYear; set => birthYear = value; }
+            public int CurrentYear { get => currentYear; set => currentYear = value; }
+
+            public int PersonAge => Convert.ToInt32(currentYear - BirthYear);
+
+            public int MaxHeartRate => 220 - PersonAge;
+
+            public int MinTargetRate => Convert.ToInt32(MaxHeartRate * 0.5);
+
+            public int MaxTargerRate => Convert.ToInt32(MaxHeartRate + 0.85);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string firstName = firstNameTextBox.Text;
+            string secondName = secondNameTextBox.Text;
+            int birthYear = Convert.ToInt32(birthYearTextBox.Text);
+            string result = "";
+
+            HeartRate heartRate = new HeartRate(firstName, secondName, birthYear);
+
+            result += "Name:  " + heartRate.FirstName + " " + heartRate.SecondName;
+            result += "\n";
+            result += "Age:  " + heartRate.PersonAge;
+            result += "\n";
+            result += "Maximum Heart Rate:  " + heartRate.MaxHeartRate;
+            result += "\n";
+            result += "Target Heart Rate:  " + (heartRate.MaxHeartRate - heartRate.MinTargetRate);
+
+            MessageBox.Show(result);
+        }
     }
 }
